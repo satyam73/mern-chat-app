20f75a3e865595fad0460e1c7e933d183671fe95
Satyam <bajpai1973satyam@gmail.com>
2024-01-03 18:14:03 +0530

    refactor:change auth middleware component to context api

---
+++ Backend/frontend/src/contexts/AuthProvider.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "./UserProvider";
+
+export const AuthContext = createContext({});
+export const useAuth = () => useContext(AuthContext);
+
+export default function Auth({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { user } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    function check() {
+
+      if (!user._id) {
+        setIsLoggedIn(false);
+        navigate("/login");
+        return;
+      }
+
+      setIsLoggedIn(true);
+    }
+
+    check();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn }}>
+      {isLoggedIn && children}
+    </AuthContext.Provider>)
+}

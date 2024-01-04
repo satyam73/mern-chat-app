900bc7bb7b39e166b295e7847fa1607a71ac0d31
Satyam <bajpai1973satyam@gmail.com>
2024-01-04 17:19:13 +0530

    refactor: remove edit button on profile page

--- Backend/frontend/src/components/NewProfilePage/NewProfilePage.js
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material';
-
-export default function NewProfilePage() {
-  return (
-    <Box>
-      <Box></Box>
-    </Box>
-  );
-}
--- Backend/frontend/src/components/ProfilePage/Profile.js
+++ Backend/frontend/src/components/ProfilePage/Profile.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import EditIcon from "@mui/icons-material/Edit";
 import {
-  IconButton,
   Box,
 } from "@mui/material";
 
-import EditModal from "./EditModal/EditModal";
 import ProfileDetailsHeader from "./ProfileDetailsHeader/ProfileDetailsHeader";
 import FriendSection from "./FriendsSection/FriendSection";
 
@@ -13,26 +10,13 @@ import styles from './profilePage.module.css';
 
 function Profile() {
   const [profile, setProfile] = useState("");
-  const [isEditPopupShowing, setIsEditPopupShowing] = useState(false);
 
   return (
     <>
-      <EditModal
-        showModal={isEditPopupShowing}
-        setShowModal={setIsEditPopupShowing}
-        profile={profile}
-        setProfile={setProfile}
-      />
       <Box
         className={styles['profile-page']}
       >
         <Box className={styles['profile-page__main']}>
-          <IconButton
-            className={styles['profile-page__edit-button']}
-            onClick={() => setIsEditPopupShowing(true)}
-          >
-            <EditIcon fontSize="medium" />
-          </IconButton>
           <ProfileDetailsHeader profile={profile}
             setProfile={setProfile} />
           <FriendSection />

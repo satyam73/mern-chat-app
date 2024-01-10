e039550068f333c9fbc81ee76a9be3e09003d041
Satyam <bajpai1973satyam@gmail.com>
2024-01-10 18:27:29 +0530

    feat: add limit for message to get last message

--- Backend/controllers/chatControllers.js
+++ Backend/controllers/chatControllers.js
@@ -15,19 +15,33 @@ const getChatByUserId = async (req, res) => {
     try {
         const { userId } = req.params;
         const self = req.user;
+        const limit = Number(req.query?.limit);
+        let chat;
 
         if (!userId && !self) {
             return res.status(403).json({
                 respose: "Fields cannot be empty!"
             })
         }
-        const chat = await Chat.find({ $and: [{ users: { $in: userId } }, { users: { $in: self._id } }] }).populate("messages");
+
+        if (limit) {
+            chat = await Chat.find({ $and: [{ users: { $in: userId } }, { users: { $in: self._id } }] }).populate({ path: "messages", options: { sort: { createdAt: -1 }, limit } });
+            return res.status(200).json({
+                response: "Chat fetched successfully!",
+                chat
+            });
+        }
+
+        chat = await Chat.find({ $and: [{ users: { $in: userId } }, { users: { $in: self._id } }] }).populate("messages");
         return res.status(200).json({
             response: "Chat fetched successfully!",
             chat
         })
     } catch (err) {
-        console.log("Error: ", err)
+        console.log('Something went wrong while fetching chats by user id ', err)
+        return res.status(500).json({
+            message: 'Internal server error'
+        });
     }
 }
 module.exports = { getAllChats, getChatByUserId }
